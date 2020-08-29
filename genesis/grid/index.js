82c46a13633221d2ee6dc4b2583fca5ef7bd23bb
clearloop <udtrokia@gmail.com>
2020-08-30 01:22:05 +0800

    chore(index): make genesis as index

---
+++ genesis/grid/index.js
@@ -0,0 +1,24 @@
+import Shape from "../../js/shape.js";
+import Frame from "../../js/frame.js";
+
+// Main function
+(function(){
+    const cols = window.innerHeight > window.innerWidth? 5: 9;
+    const rows = window.innerHeight > window.innerWidth
+          ?window.innerWidth / (window.innerHeight / cols + 1)
+          :window.innerHeight / (window.innerWidth / cols + 1) - 1;
+
+    const land = Frame.Block(cols);
+    for (let i=0; i< cols * rows; i++) {
+        const id = "block-" + i;
+        land.append("svg").attr("id", id);
+
+        for (let p = 0; p<3; p++) {
+            const conf = Shape
+                  .NewConfig(`#${id}`)
+                  .Size(window.innerWidth / (cols + 1));
+            conf.margin = 25;
+            new Shape(conf).Render();
+        }
+    }
+})();
--- js/index.js
+++ js/index.js
@@ -3,22 +3,49 @@ import Frame from "./frame.js";
 
 // Main function
 (function(){
-    const cols = window.innerHeight > window.innerWidth? 5: 9;
-    const rows = window.innerHeight > window.innerWidth
-          ?window.innerWidth / (window.innerHeight / cols + 1)
-          :window.innerHeight / (window.innerWidth / cols + 1) - 1;
-
-    const land = Frame.Block(cols);
-    for (let i=0; i< cols * rows; i++) {
-        const id = "block-" + i;
-        land.append("svg").attr("id", id);
-
-        for (let p = 0; p<3; p++) {
-            const conf = Shape
-                  .NewConfig(`#${id}`)
-                  .Size(window.innerWidth / (cols + 1));
-            conf.margin = 25;
-            new Shape(conf).Render();
-        }
-    }
+    Frame.Center()
+        .append("svg")
+        .attr("id", "land");
+    const conf = {
+        "margin": 5,
+        "selector": "#land",
+        "rotate": 0,
+        "size": 300,
+        "style": "ellipse",
+        "gradient": {
+            "enable": true,
+            "rotate": 15,
+            "style": "radialGradient",
+            "stops": [
+                {
+                    "value": "5%",
+                    "color": "rgba(136, 201, 226, 0.1)"
+                },
+                {
+                    "value": "20%",
+                    "color": "rgba(127, 44, 171, 0.8)"
+                },
+                {
+                    "value": "45%",
+                    "color": "rgba(136, 14, 238, 0.1)"
+                },
+                {
+                    "value": "75%",
+                    "color": "rgba(33, 222, 70, 0.6)"
+                },
+                {
+                    "value": "100%",
+                    "color": "rgba(12, 134, 143, 0.8)"
+                }
+            ]
+        },
+        "animate": {
+            "enable": true,
+            "rx": 5,
+            "ry": 10
+        },
+        "rx": 30,
+        "ry": 28
+    };
+    new Shape(conf).Render();
 })();
